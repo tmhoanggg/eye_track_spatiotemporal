0c28a1f26166b32ecdc7e10a6272de00479f4f12
Truong Minh Hoang <truongminhhoangtmh@gmail.com>
2025-03-25 13:58:41 +0700

    inference by 100Hz

--- generate_submission.py
+++ generate_submission.py
@@ -59,8 +59,8 @@ def main(args):
     for event_frames in event_frames_list:
         pred = streaming_inference(model, event_frames[None, :])
         pred = process_detector_prediction(pred)
-        predictions.append(pred.squeeze(0)[..., ::5])
-        #predictions.append(pred.squeeze(0))
+        #predictions.append(pred.squeeze(0)[..., ::5])
+        predictions.append(pred.squeeze(0))
         
     predictions = torch.cat(predictions, dim=-1)
 
